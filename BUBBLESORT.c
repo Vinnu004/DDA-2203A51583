a06da6fbc7684600c572d21599bbcff1f4120776
Vinnu004 <158287040+Vinnu004@users.noreply.github.com>
2024-08-01 14:29:34 +0530

    BUBBLESORT.c

--- BUBBLESORT.c
+++ BUBBLESORT.c
@@ -1,41 +1,37 @@
 #include <stdio.h>
 
-void swap(int a, int b)
-{
-    int temp = a;
-    a = b;
-    b = temp;
+void swap(int *a, int i, int j) {
+    int temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
 }
 
-void bubbleSort(int arr[], int n)
-{
-    int i, j, swapped;
-    for (i = 0; i < n - 1; i++) {
-        swapped = 0;
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(&arr[j], &arr[j + 1]);
-                swapped = 1;
+
+void bubbleSort(int arr[], int n) {
+    int i, j;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr, j, j+1);
             }
         }
-        
-        if (swapped == 0)
-            break;
     }
 }
 
-void printArray(int arr[], int size)
-{
+void printArray(int arr[], int size) {
     int i;
-    for (i = 0; i < size; i++)
+    for (i = 0; i < size; i++) {
         printf("%d ", arr[i]);
+    }
     printf("\n");
 }
 
-int main()
-{
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(arr) / sizeof(arr[0]);
+
+int main() {
+    int arr[] = {95, 46, 10, 82, 67, 11, 65};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printf("Unsorted array: \n");
+    printArray(arr, n);
     bubbleSort(arr, n);
     printf("Sorted array: \n");
     printArray(arr, n);
