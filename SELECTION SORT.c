bdbd4f180a22a3ccc5b7ce89da601e459c19add0
Vinnu004 <158287040+Vinnu004@users.noreply.github.com>
2024-08-01 14:39:44 +0530

    Add files via upload

---
+++ SELECTION SORT.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void swap(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx;
+    
+    
+ for (i = 0; i < n-1; i++) {
+
+        min_idx = i;
+        for (j = i+1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        
+     
+        swap(arr, min_idx, i);
+    }
+}
+
+
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printf("Unsorted array: \n");
+    printArray(arr, n);
+    selectionSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
